633623b37ec55eef2203192c5fe0030093f0531f
Gerardo Arzate <116304288+gerardoarzate@users.noreply.github.com>
2024-11-11 00:17:31 -0600

    Actualizar componente BackButton

--- src/presentation/components/BackButton.tsx
+++ src/presentation/components/BackButton.tsx
@@ -1,4 +1,5 @@
-import { View, Image, StyleSheet } from 'react-native';
+import { View, Image } from 'react-native';
+import { StackNavigationProp } from '@react-navigation/stack';
 import { useContext } from 'react';
 import { ThemeContext } from '../../MainApp';
 
@@ -6,16 +7,20 @@ const iconBlack = require('../img/back-arrow-black.png');
 const iconWhite = require('../img/back-arrow-white.png');
 
 interface Props {
-    onClick?: Function
+    navigation: StackNavigationProp<any>
 }
 
-export const BackButton = ({ onClick }: Props) => {
+export const BackButton = ({ navigation }: Props) => {
     const { theme } = useContext(ThemeContext);
     const isDark = theme == 'dark';
     const icon = isDark ? iconWhite : iconBlack;
 
     return (
-        <View style={{ width: 32, height: 32 }} onTouchEnd={() => { onClick?.() }}>
+        <View style={{ width: 32, height: 32 }} onTouchEnd={() => {
+            if (navigation?.canGoBack()) {
+                navigation.goBack();
+            }
+         }}>
             <Image style={{ width: '100%', height: '100%' }} source={icon}/>
         </View>
     );
