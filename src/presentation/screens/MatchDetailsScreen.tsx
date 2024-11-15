8368d873266d401682d21c628c1620b7af781cd1
Gerardo Arzate <116304288+gerardoarzate@users.noreply.github.com>
2024-11-15 11:34:34 -0600

    Conectar la API a la pantalla de detalles del partido

--- src/presentation/screens/MatchDetailsScreen.tsx
+++ src/presentation/screens/MatchDetailsScreen.tsx
@@ -1,16 +1,25 @@
-import { FlatList, StyleSheet, Text, View, Image } from 'react-native';
-import { Picker } from '@react-native-picker/picker';
-import { useContext, useState } from 'react';
+import { Alert, StyleSheet, Text, ActivityIndicator } from 'react-native';
+import { useContext, useEffect, useState } from 'react';
 import { RootStackParams } from '../navigation/StackNavigator';
 import { StackScreenProps } from '@react-navigation/stack';
 import { Header } from '../components/Header';
 import { MatchCard } from '../components/MatchCard';
 import { BackButton } from '../components/BackButton';
-import { ThemeContext } from '../../MainApp';
+import { ThemeContext, APIContext } from '../../MainApp';
 import { VictoryProbabilityBar } from '../components/VictoryProbabilityBar';
 import { teams } from '../../data/teams';
 
 interface Props extends StackScreenProps<RootStackParams, 'MatchDetailsScreen'> {}
+
+interface IPrediction {
+    probabilidad_equipo_1: number,
+    probalidad_empate: number,
+    probabilidad_equipo_2: number,
+    nombre_equipo_local: string,
+    nombre_equipo_visitante: string,
+    goles_promedio: number,
+    corners_promedio: number
+}
 
 export const MatchDetailsScreen = ({ navigation, route }: Props) => {
     const { leftTeamId, rightTeamId } = route.params;
@@ -24,70 +33,76 @@ export const MatchDetailsScreen = ({ navigation, route }: Props) => {
     const { theme } = useContext(ThemeContext);
     const isDark = theme == 'dark';
 
+    const { apiUrl } = useContext(APIContext);
+    const [prediction, setPrediction] = useState<undefined | IPrediction>();
+
+    useEffect(() => {
+        const body = new FormData();
+        body.append('home_team', leftTeam.id);
+        body.append('away_team', rightTeam.id);
+
+        const res = fetch(`${apiUrl}/predict`, {
+            method: 'POST',
+            body: body
+        })
+        .then(async res => {
+            if (!res.ok) {
+                throw new Error(`Respuesta ${res.status}\n${await res.text()}`);
+            }
+
+            const json = await res.json();
+            console.log(json);
+            setPrediction(json);
+        })
+        .catch(error => {
+            Alert.alert('Error al predecir', error.message);
+        });
+    }, []);
+
     const styles = StyleSheet.create({
         text: {
             fontSize: 18,
             color: isDark ? 'white' : 'black',
         },
-        teamPickerContainer: {
-            display: 'flex',
-            flexDirection: 'row',
-            alignItems: 'center',
-            paddingLeft: 16
-        },
-        teamPickerText: {
-            width: '50%',
-            fontSize: 18,
-            color: isDark ? 'white' : 'black'
-        },
-        bigLogo: {
-            width: 120,
-            height: 120,
-            marginHorizontal: 'auto',
-            marginVertical: 16
-        },
         predictionLabel: {
             fontSize: 18,
             color: isDark ? 'white' : 'black',
             textAlign: 'center',
-            marginVertical: 12
+            marginVertical: 16
         },
         probabilityBar: {
             marginTop: 16,
-            marginBottom: 32
+            marginBottom: 40
+        },
+        activityIndicator: {
+            width: 30,
+            height: 30,
+            margin: 'auto'
         }
     });
-
-    const [selectedTeamId, setSelectedTeamId] = useState(leftTeam.id);
-    const selectedTeam = leftTeam.id == selectedTeamId ? leftTeam : rightTeam;
 
     return (
         <>
             <Header />
             <BackButton />
             <MatchCard leftTeamLogo={leftTeam.logo} rightTeamLogo={rightTeam.logo} />
-            <VictoryProbabilityBar
-                leftTeamName={leftTeam.name}
-                leftTeamProbability={53.2}
-                rightTeamName={rightTeam.name}
-                rightTeamProbability={32.3}
-                style={styles.probabilityBar}
-            />
-            <View style={styles.teamPickerContainer}>
-                <Text style={styles.teamPickerText}>Predicción a partir del</Text>
-                <Picker
-                    style={styles.teamPickerText}
-                    selectedValue={selectedTeamId}
-                    onValueChange={setSelectedTeamId}
-                >
-                    <Picker.Item label={leftTeam.name} value={leftTeam.id} />
-                    <Picker.Item label={rightTeam.name} value={rightTeam.id} />
-                </Picker>
-            </View>
-            <Image style={styles.bigLogo} source={selectedTeam.logo} resizeMode='contain' />
-            <Text style={styles.predictionLabel}>Probabilidad de victoria: 53.2%</Text>
-            <Text style={styles.predictionLabel}>Goles promedio del partido: 2.8</Text>
-            <Text style={styles.predictionLabel}>Corners promedio del partido: 18.3</Text>
+            { prediction ? (
+                <>
+                    <Text style={styles.predictionLabel}>Probabilidad de victoria</Text>
+                    <VictoryProbabilityBar
+                        leftTeamName={prediction.nombre_equipo_local}
+                        leftTeamProbability={prediction.probabilidad_equipo_1}
+                        rightTeamName={prediction.nombre_equipo_visitante}
+                        rightTeamProbability={prediction.probabilidad_equipo_2}
+                        drawProbability={prediction.probalidad_empate}
+                        style={styles.probabilityBar}
+                    />
+                    <Text style={styles.predictionLabel}>Goles promedio del partido: {prediction.goles_promedio}</Text>
+                    <Text style={styles.predictionLabel}>Corners promedio del partido: {prediction.corners_promedio}</Text>
+                </>
+            ) : (
+                <ActivityIndicator style={styles.activityIndicator} />
+            )}
         </>
     );
 };
